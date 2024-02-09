1e325192a2599401a303607731355edebdb76c57
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-09 18:24:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,19 @@ import streamlit as st
 st.title("🦋Hi, I'm Nunzia!")
 # st.write("")
 st.info("Data Science & Machine Learning || University of Fisciano, Italy || Web Developer Freelancer", icon="💻")
-st.image('iamges/memoji.webp', caption='Sunrise by the mountains')
 
+with col1:
+    st.image('iamges/memoji.webp', caption='Sunrise by the mountains')
 
-# Controls whether the default sidebar page navigation in a multipage app is
-# displayed.
+# Controls whether the default sidebar page navigation in a multipage app is displayed.
 # Default: true
 showSidebarNavigation = true
 
 # Sidebar for MENU
 with st.sidebar:
-  st.write("☰ MENU")
-  st.page_link("streamlit_app.py", label="Home", icon="🏡")
-  st.page_link("pages/echoBot.py", label="Echo Chat", icon="🤖")
+    st.write("☰ MENU")
+    st.page_link("streamlit_app.py", label="Home", icon="🏡")
+    st.page_link("pages/echoBot.py", label="Echo Chat", icon="🤖")
 
 # draw celebratory balloons.
 st.balloons()
