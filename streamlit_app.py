ca7160b6fc740cbab91488e7a4d1afde29f16118
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-09 17:41:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,5 @@ st.title("🦋Hi, I'm Nunzia!")
 #st.write("")
 st.info("Application Test", icon="💻")
 
-# siderbar_text =
-st.page_link("streamlit_app.py", label="Home", icon="🏡")
-st.page_link("pages/echoBot.py", label="Echo Chat", icon="🤖")
+siderbar_list = st.page_link("streamlit_app.py", label="Home", icon="🏡")
+siderbar_list = st.page_link("pages/echoBot.py", label="Echo Chat", icon="🤖")
