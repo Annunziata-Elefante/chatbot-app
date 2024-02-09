0554833ded530e125005feb42e4352bf0d884620
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-09 18:46:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.title("🦋Hi, I'm Nunzia!")
 # st.write("")
 st.info("Data Science & Machine Learning || University of Fisciano, Italy || Web Developer Freelancer", icon="💻")
 
+col1, col2 = st.columns(2)
 with col1:
     st.image('images/memoji.webp', caption='Sunrise by the mountains')
 
