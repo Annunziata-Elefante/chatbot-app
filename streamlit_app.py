8831c509fdf5feffe0a8e79f6b2c67274eadb8bf
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-09 18:05:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ st.title("🦋Hi, I'm Nunzia!")
 #st.write("")
 st.info("Data Science & Machine Learning || University of Fisciano, Italy || Web Developer Freelancer", icon="💻")
 
+# Controls whether the default sidebar page navigation in a multipage app is
+# displayed.
+# Default: true
+showSidebarNavigation = true
+
 # Sidebar with MENU
 with st.sidebar:
   st.page_link("streamlit_app.py", label="Home", icon="🏡")
