ee8145a79687e2fdca614f3455cb8ee0b5bd05a4
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-10 19:14:54 +0200

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -49,8 +49,8 @@ with st.container():
         st.write("THESIS: 'Inference of Sensitive Information in Intelligent Environments through the Analysis of Trigger-Action Rules.'")
         st.write("FINAL GRADE: 110/110 cum laude")
 
-
     st.divider()
+    
     c1, c2 = st.columns([1, 3])
     with c1:
         st.caption("2021 - 2023")
@@ -59,3 +59,44 @@ with st.container():
         st.write("_University of Salerno_")
         st.write("THESIS: 'Development of front-end services for the EcoGrowth application.'")
         st.write("FINAL GRADE: 110/110 cum laude")
+
+# ------------------------- RESEARCH PAPERS -------------------------
+st.markdown("<h2 style='text-align: center; color: black;'> RESEARCH PAPERS 📑 </h2>", unsafe_allow_html=True)
+with st.container():
+    cl1, cl2 = st.columns([1, 3])
+    with cl1:
+        st.caption("[4]")
+    with cl2:
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
+        st.write("'Unleashing the Power of NLP Models for Semantic Consistency Checking of Automation Rules',")
+        st.write("Proceedings of the Journal of Visual Language and Computing (JVLC) Vol. 2023 No.2, December 2023, pp.1-14")
+        
+    st.divider()
+    
+    c1, c2 = st.columns([1, 3])
+    with c1:
+        st.caption("[3]")
+    with c2:
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
+        st.write("'A BERT-based Model for Semantic Consistency Checking of Automation Rules',")
+        st.write("Proceedings of 29th International DMS Conference on Visualization and Visual Languages (DMSVIVA 2023), San Francisco, 29-30 June, 2023, pp. 87-93, ISSN: 2326-3261")
+
+    st.divider()
+    
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        st.caption("[2]")
+    with col2:
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
+        st.write("'User Perception of Risks Associated with IFTTT Applets: A Preliminary User Study',")
+        st.write("Proceedings of the Italian Conference on Cybersecurity (ITASEC2023), Bari, 2-5 May, 2023, to appear in CEUR Workshop Proceedings, pp. 1-12")
+        
+    st.divider()
+
+    col3, col4 = st.columns([1, 3])
+    with col3:
+        st.caption("[1]")
+    with col4:
+        st.write("B. Breve, G. Cimino, G. Desolda, V. Deufemia, A. Elefante,")
+        st.write("'On the User Perception of Security Risks of Trigger-Action Rules: A User Study',")
+        st.write("Proceedings of the 9th International Symposium on End-User Development (IS-EUD 2023). Cagliari, 6-8 June, 2023, Lecture Notes in Computer Science (LNCS) 13917, 2023, Lucio Davide Spano et al. (Eds.), pp. 162–179, Springer-Verlag")
