50cb35950a5ee842fed2b864fd8fc9c64673a2f3
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-10 10:01:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,15 @@ st.title("🦋Hi, I'm Nunzia!")
 # st.write("")
 st.info("Data Science & Machine Learning || University of Fisciano, Italy || Web Developer Freelancer", icon="💻")
 
+'''
 col1, col2 = st.columns(2)
 with col1:
     st.image('images/memoji.webp', caption='Nunzia')
+'''
 
 # Sidebar for MENU
 with st.sidebar:
+    st.image('images/profile.JPG', caption='Nunzia')
     st.write("☰ MENU")
     st.page_link("streamlit_app.py", label="Home", icon="🏡")
     st.page_link("pages/echoBot.py", label="Echo Chat", icon="🤖")
