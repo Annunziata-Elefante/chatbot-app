fc0569fb29f61143c1660dfe24f7306359d11c67
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-10 11:38:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,15 @@ import streamlit as st
 
 
 # PROFILE
-col1, col2 = st.columns([3, 2])
+col1, col2 = st.columns(2)
 with col1:
-    st.header("Hey there.")
-    st.header("I'm Annunziata Elefante!")
-    st.write("I'm a programmer with a specialization in Data Science and Machine Learning, and Web Developer Freelancer")
+    st.image('images/photo_profile.webp')                # caption='Annunziata Elefante'   
     # st.write("Hi there 👋 My name is Annunziata Elefante, but my friends call me :black[Nunzia]. \n I'm a 25-year-old programmer with a specialization in Data Science and Machine Learning. \n\n")
 with col2:
-    st.image('images/profile.JPG')                # caption='Annunziata Elefante'    
+    st.header("Hey there. 🦋")
+    st.header("I'm Annunziata Elefante!")
+    st.write("I'm a programmer with a specialization in Data Science and Machine Learning, and Web Developer Freelancer")
+     
 
 # SIDEBAR
 with st.sidebar:
