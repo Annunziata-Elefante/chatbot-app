91215d110dacae0dabd397307ddad25bf869c195
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-10 12:06:31 +0100

    Add constant.py file

---
+++ constant.py
@@ -0,0 +1,28 @@
+info = {
+   "Pronoun": "her", 
+   "Name": "Annunziata",
+   "Full_Name": "Annunziata Elefante",
+   "Intro": "A programmer with expertise in Data Science and Machine Learning",
+   "About":"Hi there, I'm Annunziata! I'm a 25-year-old with a keen interest in web development. Having spent 2 years as a freelance researcher and web developer, I thrive on diving into programming projects. During my leisure time, you'll often find me enjoying the company of cats and immersing myself in captivating films",
+   "City":"Sant'Antonio Abate (NA), Italy",
+   "Email": "nunziaelefante@mail.com"
+}
+
+'''
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
+'''
