d9601953e24041b1f83698d755816d3f8e7a4119
Nunzia <159291489+Annunziata-Elefante@users.noreply.github.com>
2024-02-12 11:27:51 +0200

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -47,14 +47,14 @@ with st.container():
         st.write("MASTER'S DEGREE IN COMPUTER SCIENCE (LM-18)")
         st.write("_University of Salerno_")
         
-    st.markdown("<p style='text-align: center; color: black;'> ________________ </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black;'> 󠀾 </p>", unsafe_allow_html=True)
     
     c1, c2 = st.columns([1, 3])
     with c1:
         st.caption("2017 - 2021")
     with c2:
         st.write("BACHELOR'S DEGREE IN COMPUTER SCIENCE (L-31)")
-        st.write("_University of Salerno_")        
+        st.write("_University of Salerno_ \nTHESIS: '_Inference of Sensitive Information in Intelligent Environments through the Analysis of Trigger-Action Rules._' \nFINAL GRADE: 110/110 cum laude")        
 
 st.divider()
 
@@ -78,30 +78,22 @@ with st.container():
     with cl1:
         st.caption("[4]")
     with cl2:
-        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
-        st.write("'_Unleashing the Power of NLP Models for Semantic Consistency Checking of Automation Rules_',")
-        st.write("Proceedings of the Journal of Visual Language and Computing (JVLC) Vol. 2023 No.2, December 2023, pp.1-14")
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante, '_Unleashing the Power of NLP Models for Semantic Consistency Checking of Automation Rules_', st.write("Proceedings of the Journal of Visual Language and Computing (JVLC) Vol. 2023 No.2, December 2023, pp.1-14")
             
     c1, c2 = st.columns([1, 3])
     with c1:
         st.caption("[3]")
     with c2:
-        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
-        st.write("'_A BERT-based Model for Semantic Consistency Checking of Automation Rules_',")
-        st.write("Proceedings of 29th International DMS Conference on Visualization and Visual Languages (DMSVIVA 2023), San Francisco, 29-30 June, 2023, pp. 87-93, ISSN: 2326-3261")
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante, '_A BERT-based Model for Semantic Consistency Checking of Automation Rules_', "Proceedings of 29th International DMS Conference on Visualization and Visual Languages (DMSVIVA 2023), San Francisco, 29-30 June, 2023, pp. 87-93, ISSN: 2326-3261")
     
     col1, col2 = st.columns([1, 3])
     with col1:
         st.caption("[2]")
     with col2:
-        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante,")
-        st.write("'_User Perception of Risks Associated with IFTTT Applets: A Preliminary User Study_',")
-        st.write("Proceedings of the Italian Conference on Cybersecurity (ITASEC2023), Bari, 2-5 May, 2023, to appear in CEUR Workshop Proceedings, pp. 1-12")
+        st.write("B. Breve, G. Cimino, V. Deufemia, A. Elefante, st.write("'_User Perception of Risks Associated with IFTTT Applets: A Preliminary User Study_', st.write("Proceedings of the Italian Conference on Cybersecurity (ITASEC2023), Bari, 2-5 May, 2023, to appear in CEUR Workshop Proceedings, pp. 1-12")
         
     col3, col4 = st.columns([1, 3])
     with col3:
         st.caption("[1]")
     with col4:
-        st.write("B. Breve, G. Cimino, G. Desolda, V. Deufemia, A. Elefante,")
-        st.write("'_On the User Perception of Security Risks of Trigger-Action Rules: A User Study_',")
-        st.write("Proceedings of the 9th International Symposium on End-User Development (IS-EUD 2023). Cagliari, 6-8 June, 2023, Lecture Notes in Computer Science (LNCS) 13917, 2023, Lucio Davide Spano et al. (Eds.), pp. 162–179, Springer-Verlag")
+        st.write("B. Breve, G. Cimino, G. Desolda, V. Deufemia, A. Elefante, '_On the User Perception of Security Risks of Trigger-Action Rules: A User Study_', "Proceedings of the 9th International Symposium on End-User Development (IS-EUD 2023). Cagliari, 6-8 June, 2023, Lecture Notes in Computer Science (LNCS) 13917, 2023, Lucio Davide Spano et al. (Eds.), pp. 162–179, Springer-Verlag")
